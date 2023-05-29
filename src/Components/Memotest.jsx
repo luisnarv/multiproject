44d654e2d9d48c9da3537df94c701d26480a6d86
luisnarv <115510650+luisnarv@users.noreply.github.com>
2023-05-29 14:01:19 -0500

    Memotest

--- src/Components/Memotest.jsx
+++ src/Components/Memotest.jsx
@@ -1,28 +1,12 @@
 import { useEffect, useState } from "react";
 import { img } from "./Memotes";
+import { Link } from "react-router-dom";
 import "./Memotest.scss";
-
-
-// const img = [
-//     " https://icongr.am/devicon/android-original.svg?size=100&color=currentColor",
-//     "https://icongr.am/devicon/css3-original.svg?size=100&color=currentColor",
-//     "https://icongr.am/devicon/gitlab-original-wordmark.svg?size=100&color=currentColor",
-//     "https://icongr.am/devicon/go-original.svg?size=100&color=currentColor",
-//     "https://icongr.am/devicon/javascript-original.svg?size=100&color=currentColor",
-//     "https://icongr.am/devicon/nodejs-original.svg?size=100&color=currentColor",
-//     "https://icongr.am/devicon/sequelize-original.svg?size=100&color=currentColor",
-//     "https://icongr.am/devicon/visualstudio-plain.svg?size=100&color=currentColor",
-//     "https://icongr.am/devicon/windows8-original.svg?size=100&color=currentColor",
-//     "https://icongr.am/devicon/react-original.svg?size=100&color=currentColor",
-// ].flatMap((image) => [`a|${image}`, `b|${image}`]
-// ).sort(() => Math.random() - 0.5);
-
-
 
 export default function Memotest() {
     const [leaveState, setLeaveState] = useState(false);
     const handleMouseEnter = () => setLeaveState(!leaveState);
-    const [Count, setCount] = useState(0)
+    const [CountImage, setCountImage] = useState(0)
     const [Search, setSearch] = useState([]);
     const [Select, setSelect] = useState([]);
     const [Time, setTime] = useState(0);
@@ -33,9 +17,25 @@ export default function Memotest() {
     const userImage = JSON.parse(localStorage.getItem("imageload"));
 
     useEffect(() => {
+        if (userImage) {
+            if (userImage[0].includes("a|") || userImage[0].includes("b|")) {
+                const image = userImage.filter(e => e.includes("a|"))
+                const image2 = image.map(image => image.slice(2, image.length))
+                setImageload(image2);
+                setCountImage(image2.length)
+            } else {
+                const image = userImage
+                setImageload(image)
+                setCountImage(image.length)
+            }
+        }
+    }, [0])
+
+
+    useEffect(() => {
         if (Select.length === 2) {
             if (Select[0].split("|")[1] === Select[1].split("|")[1]) {
-            
+
                 setSearch((Search) => Search.concat(Select))
                 setSelect([])
             } else {
@@ -43,7 +43,7 @@ export default function Memotest() {
             }
         };
 
-        if (Search.length === img.length) {
+        if (Search.length === img.length && Play === true) {
             alert("you win!!")
             location.reload()
         }
@@ -62,14 +62,12 @@ export default function Memotest() {
     }, [Select, Time])
 
 
-
-
     function handleimage(e) {
-        setCount(Count +1)
-        e.preventDefault()
-        setImageload([...Imageload, Input])
-        setInput("")
-        localStorage.setItem("imageload", JSON.stringify(Image))
+        e.preventDefault();
+        setImageload([...Imageload, Input]);
+        setCountImage(Imageload.length + 1);
+        setInput("");
+        localStorage.setItem("imageload", JSON.stringify(Image));
     }
 
 
@@ -79,10 +77,45 @@ export default function Memotest() {
         setTime(60);
     }
 
-    function handlefunctionPlayUser(e) {
+    function handlefunctionPlayUserdata(e) {
         e.preventDefault();
+        localStorage.setItem("imageload", JSON.stringify(Image));
         setPlay(false);
         setTime(60);
+    }
+
+    function handleFilter(e, id) {
+        e.preventDefault()
+        setImageload(Imageload.filter(image => image !== id));
+        setCountImage(Imageload.length - 1)
+        localStorage.setItem("imageload", JSON.stringify(Imageload.filter(image => image !== id)));
+    }
+
+    const help = (
+        <div>
+            <p>Para ingresar imágenes debes agregar 15 URL de imágenes, busca las imágenes de tu preferencia en Google has click derecho en la imagen, dar click en abrir imagen en una nueva ventana, puedes copiar la URL que te muestra en la parte de arriba.</p>
+            <hr />
+            <br />
+            <hr />
+            <p>También puedes agregar iconos del siguiente enlace  <Link to={"https://icongr.am/"} target="_blank" >https://icongr.am/</Link> para agregar al juego, eliges el tema de tu preferencia
+                seleccionas el icono que prefieres das click del lado derecho te aparece una ventana con la URL que debes insertar. </p>
+            <br />
+            <p>Nota: tamaño de la image no más de 700px.</p>
+        </div>
+
+    )
+
+    function handleInputChange(event) {
+        const inputElement = event.target;
+        const addima = document.getElementById("addima");
+        const inputValue = inputElement.value;
+
+        if (inputValue !== "") {
+            addima.classList.add("animateOpen");
+        } else {
+            addima.classList.remove("animateOpen");
+            addima.classList.add("animateClose");
+        }
     }
 
 
@@ -92,36 +125,53 @@ export default function Memotest() {
                 <div className="container" >
                     <div>
                         <button type="submit" onClick={(e) => handlefunctionPlay(e)} >Play</button>
-                        {Imageload.length < 10 || Imageload.length > 10 ? <button type="" disabled>Play</button> : <button type="submit" onClick={(e) => handlefunctionPlayUser(e)} >Play  </button>
+                        {Imageload.length === 15 ?
+                            <button type="submit" onClick={(e) => handlefunctionPlayUserdata(e)} >Play </button>
+                            : Imageload.length === 10 ?
+                                <button type="submit" onClick={(e) => handlefunctionPlayUserdata(e)} >Play </button>
+                                : <button style={{ cursor: "no-drop" }} type="" disabled>Play</button>
                         }
                     </div>
                     <div>
                         <form className="formulario" action="submit" onSubmit={(e) => handleimage(e)} >
                             <h3>¿Quieres ingresar tus imagenes?</h3>
-                            <div><p>Imagenes cargadas: {Count}</p>
                             <button onMouseEnter={handleMouseEnter} style={{
                                 borderRadius: "50%",
                                 width: "30px",
                                 height: "30px",
                             }} type=""> <span
                                 style={{ display: "flex", width: "25px", fontSize: "15px", justifyContent: "center" }}
-                            >?</span></button></div>
-                            
-                        { leaveState &&    <div className="nohelp" onMouseLeave={handleMouseEnter} >
-                                <p>Para ingresar imágenes debes agregar 10 URL de imágenes de Google
-                                    busca las imágenes de tu preferencia en Google has clic derecho en la imagen
-                                    Y dar clic en abrir imagen en una nueva ventana, puedes copiar la URL que te muestra en la parte de arriba.</p>
-                                    <hr />
-                                    <br />
-                                    <hr />
-                                <p>También puedes agregar iconos del siguiente enlace "https://icongr.am/" para agregar al juego, eliges el tema de tu preferencia
-                                    seleccionas el icono que prefieres das clic del lado derecho te aparece una ventana con la URL que debes insertar </p>
-                                    <br />
-                                    <p>Nota: tamaño de la image no más de 700px</p>
+                            >?</span></button>
+                            <div>
+                                <p>Imagenes cargadas: {CountImage}</p>
+
+                                <div className="container image">
+                                    <ul>
+                                        {Imageload.map((image, i) => {
+                                            return (
+                                                <li className="lista" key={i} >
+                                                    <button onClick={(e) => handleFilter(e, image)} className="close"> <span> <img style={{ width: "100%" }} src="https://icongr.am/fontawesome/close.svg?size=100&color=currentColor" alt="icon-close" /> </span> </button>
+                                                    <img src={`${image}`} alt="" />
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                </div>
+
                             </div>
-}
-                            <input value={Input} type="text" onChange={(e) => setInput(e.target.value)} />
-                            <button type="submit">Ingresar</button>
+
+                            {leaveState &&
+                                <div className="help" onMouseLeave={handleMouseEnter} >
+                                    {help}
+                                </div>
+                            }
+                            <div id="divin">
+                                <label id="addima" className="labe" >Add Image</label>
+                                {Imageload.length >= 15 ? <input id="myInput" value={Input} type="text" onInput={(event) => handleInputChange(event)} onChange={(e) => setInput(e.target.value)} aria-describedby="Ingresar imagen" placeholder="Ingresar imagen" disabled /> :
+                                    <input id="myInput" value={Input} type="text" onInput={(event) => handleInputChange(event)} onChange={(e) => setInput(e.target.value)} aria-describedby="Ingresar imagen" placeholder="Ingresar imagen" />}
+                            </div>
+                            {Imageload.length >= 15 ? <button type="submit" disabled>Ingresar</button>
+                                : <button type="submit">Ingresar</button>}
                         </form>
                     </div>
                 </div>
@@ -147,7 +197,6 @@ export default function Memotest() {
                             })
                                 : img.map((e) => {
                                     const [, url] = e.split("|");
-
                                     return (
                                         <li
                                             key={e}
@@ -161,13 +210,10 @@ export default function Memotest() {
                                     )
                                 })
                             }
-
-
-
                         </ul>
-
                     </div>
                 </>
-            }       </>
+            }
+        </>
     )
 }
